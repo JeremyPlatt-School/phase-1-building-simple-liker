3b04c17a0811d81c94f83e6f9d6be7f50315c2c5
Jeremy Platt <jplatt4427@gmail.com>
2022-05-08 15:42:26 -0700

    completed assignment

--- main.js
+++ main.js
@@ -3,9 +3,41 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let heart = document.querySelectorAll(".like-glyph");
 
 
 
+for (const icon of heart) {
+  icon.addEventListener("click", likeButton);
+ }
+
+
+function likeButton(heart){
+  let icon = heart.target;
+  mimicServerCall()
+    .then(function(serverMessage){
+      if(icon.classList.contains("activated-heart")) {
+        icon.classList.remove("activated-heart");
+        console.log("heart TURNED OFF")
+    } else(icon.classList.add("activated-heart"));
+      console.log(serverMessage);
+    })
+
+
+    .catch(function(error) {
+      document.getElementById("modal").classList.remove("hidden");
+      setTimeout(function(){
+        console.log("TimeOutReached")
+        document.getElementById("modal").classList.add("hidden");
+    }, 3000);
+      console.log(error);
+    });
+  
+}
+
+// let par = document.createElement("p");
+// par.textContent = error;
+// document.getElementById("modal").appendChild(par);
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
